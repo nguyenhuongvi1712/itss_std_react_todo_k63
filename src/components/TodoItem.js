cde5808a3f50e799269779f43545e619ab1d3f10
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2022-06-02 15:42:28 +0700

    Create by other components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,13 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  ) {
-  return (
-    <label className="panel-block">
-    </label>
-  );
+function TodoItem(props) {
+	return (
+		<label className="panel-block">
+			<input type="checkbox" />
+			{props.item.text}
+		</label>
+	);
 }
 
 export default TodoItem;
